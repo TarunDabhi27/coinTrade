c4a6a86dfe73f96d2357f02c6eee33a9e63764e8
Tarun Dabhi <tarundabhi@gmail.com>
2021-09-04 19:45:27 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-      
+        <Link to='/coinTrade'><PebbleLabel labelName={text.home} style={{color: colorCode.green}}></PebbleLabel></Link>
         <Link to='/'><PebbleLabel labelName={text.home} style={{color: colorCode.green}}></PebbleLabel></Link>
         <Link to='/about'><PebbleLabel labelName={text.about} style={{color: colorCode.green}}></PebbleLabel></Link>
         <Link to='/login'><PebbleLabel labelName={text.login} style={{color: colorCode.green}}></PebbleLabel></Link>
